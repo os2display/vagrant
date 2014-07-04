79b49a0e29a52b4f824c99c2e567451242c1f7dc
Thomas Johansen <thsj@aarhus.dk>
2014-07-04 09:26:21 +0200

    Added search_node repos

--- setup_htdocs.sh
+++ setup_htdocs.sh
@@ -1,4 +1,5 @@
 mkdir htdocs
 cd htdocs
 git clone git@github.com:Indholdskanalen/indholdskanalen.git backend_indholdskanalen
+git clone git@github.com:Indholdskanalen/search_node.git search_node
 cd ..
