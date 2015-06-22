fac28b61d4f623ac727764d0675f0bae6babd18c
Jesper Kristensen <cableman@linuxdev.dk>
2015-06-22 14:40:36 +0200

    Moved search node on github, so update sh file in indholdskanalen

--- setup_htdocs.sh
+++ setup_htdocs.sh
@@ -1,7 +1,7 @@
 mkdir htdocs
 cd htdocs
 git clone git@github.com:aroskanalen/admin.git admin
-git clone git@github.com:aroskanalen/search_node.git search_node
+git clone git@github.com:search-node/search_node.git search_node
 git clone git@github.com:aroskanalen/middleware.git middleware
 git clone git@github.com:aroskanalen/screen.git screen
 cd ..
